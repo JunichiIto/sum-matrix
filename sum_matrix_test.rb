efa8c67d25cfce647c241c5dbfcdefffd0700ad4
Junichi Ito <jit@sonicgarden.jp>
2015-04-03 08:44:30 +0900

    Add test.

---
+++ sum_matrix_test.rb
@@ -0,0 +1,27 @@
+require "minitest/autorun"
+
+class TestMeme < Minitest::Test
+  def sum_matrix(matrix)
+    []
+  end
+
+  def test_hoge
+    input = [
+        [9,85,92,20],
+        [68,25,80,55],
+        [43,96,71,73],
+        [43,19,20,87],
+        [95,66,73,62]
+    ]
+    expected = [
+        [9,85,92,20,206],
+        [68,25,80,55,228],
+        [43,96,71,73,283],
+        [43,19,20,87,169],
+        [95,66,73,62,296],
+        [258,291,336,297,1182]
+    ]
+
+    assert_equal expected, sum_matrix(input)
+  end
+end
