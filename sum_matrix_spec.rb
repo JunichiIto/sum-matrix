e60d33f0c4034e8a9619005484393067e198400d
Junichi Ito <jit@sonicgarden.jp>
2015-04-04 11:51:48 +0900

    Convert to RSpec.

--- sum_matrix_spec.rb
+++ sum_matrix_spec.rb
@@ -1,5 +1,3 @@
-require 'minitest/autorun'
-
 module SumMatrix
   extend self
 
@@ -30,8 +28,8 @@ module SumMatrix
   end
 end
 
-class TestSumMatrix < Minitest::Test
-  def test_sum_matrix
+describe SumMatrix do
+  example "sum_matrix" do
     input = [
         [9, 85, 92, 20],
         [68, 25, 80, 55],
@@ -48,10 +46,10 @@ class TestSumMatrix < Minitest::Test
         [258, 291, 336, 297, 1182]
     ]
 
-    assert_equal expected, SumMatrix.sum_matrix(input)
+    expect(SumMatrix.sum_matrix(input)).to eq expected
   end
 
-  def test_format_matrix_max_400
+  example "format_matrix_max_400" do
     input = [
         [1, 2, 3, 4],
         [100, 200, 300, 400]
@@ -61,10 +59,10 @@ class TestSumMatrix < Minitest::Test
 100|200|300|400
     TEXT
 
-    assert_equal expected, SumMatrix.format_matrix(input)
+    expect(SumMatrix.format_matrix(input)).to eq expected
   end
 
-  def test_format_matrix_max_40
+  example "format_matrix_max_40" do
     input = [
         [1, 2, 3, 4],
         [10, 20, 30, 40]
@@ -74,23 +72,23 @@ class TestSumMatrix < Minitest::Test
 10|20|30|40
     TEXT
 
-    assert_equal expected, SumMatrix.format_matrix(input)
+    expect(SumMatrix.format_matrix(input)).to eq expected
   end
 
-  def test_generate_matrix
+  example "generate_matrix" do
     matrix = SumMatrix.generate_matrix(col: 4, row: 5, number_range: 1..99)
-    assert_equal 5, matrix.size
-    assert matrix.all?{|row| row.size == 4 }
-    assert matrix.flatten.all?{|n| n.between?(1, 99) }
+    expect(matrix.size).to eq 5
+    expect(matrix.all?{|row| row.size == 4 }).to be_truthy
+    expect(matrix.flatten.all?{|n| n.between?(1, 99) }).to be_truthy
 
     matrix2 = SumMatrix.generate_matrix(col: 4, row: 5, number_range: 1..99)
-    assert matrix != matrix2, 'ランダムな結果が得られること'
+    expect(matrix != matrix2).to be_truthy, 'ランダムな結果が得られること'
   end
 
   # テストしたいというよりも結果が見たいだけ
-  def test_generate_sum_matrix
+  example "generate_sum_matrix" do
     result = SumMatrix.generate_sum_matrix
     puts result
-    assert result.is_a?(String)
+    expect(result.is_a?(String)).to be_truthy
   end
 end
